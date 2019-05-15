b40d966649f327e1b40903150e9b94f300156abc
丁敬 <dingjing@live.cn>
2019-05-15 15:16:28 +0800

    更新 uu234 链接

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
 
     """ www.uu234.cc 开始 """
     ccuu234 = spiderFactory.get_spider('cc_uu234')
-    ccuu234.set_seed_url('www.uu234.cc/xuanhuan/list_update_', 1, 75, '.html')         # 玄幻魔法
-    ccuu234.set_seed_url('www.uu234.cc/wuxia/list_update_', 1, 38, '.html')            # 武侠修真
-    ccuu234.set_seed_url('www.uu234.cc/dushi/list_update_', 1, 105, '.html')           # 现代都市
-    ccuu234.set_seed_url('www.uu234.cc/yanqing/list_update_', 1, 82, '.html')          # 言情小说
-    ccuu234.set_seed_url('www.uu234.cc/junshi/list_update_', 1, 11, '.html')           # 历史军事
-    ccuu234.set_seed_url('www.uu234.cc/jingji/list_update_', 1, 18, '.html')           # 游戏竞技
-    ccuu234.set_seed_url('www.uu234.cc/kehuan/list_update_', 1, 32, '.html')           # 科幻灵异
-    ccuu234.set_seed_url('www.uu234.cc/shenmei/list_update_', 1, 8, '.html')           # 耽美小说
-    ccuu234.set_seed_url('www.uu234.cc/tongren/list_update_', 1, 7, '.html')           # 同人小说
-    ccuu234.set_seed_url('www.uu234.cc/qita/list_update_', 1, 65, '.html')             # 其它小说
-    ccuu234.set_seed_url('http://www.uu234.cc/quanben/list_', 1, 390, '.html')         # 全本书籍
+    ccuu234.set_seed_url('http://www.uu234.cc/xuanhuan/list_update_', 1, 75, '.html')           # 玄幻魔法
+    ccuu234.set_seed_url('http://www.uu234.cc/wuxia/list_update_', 1, 38, '.html')              # 武侠修真
+    ccuu234.set_seed_url('http://www.uu234.cc/dushi/list_update_', 1, 105, '.html')             # 现代都市
+    ccuu234.set_seed_url('http://www.uu234.cc/yanqing/list_update_', 1, 82, '.html')            # 言情小说
+    ccuu234.set_seed_url('http://www.uu234.cc/junshi/list_update_', 1, 11, '.html')             # 历史军事
+    ccuu234.set_seed_url('http://www.uu234.cc/jingji/list_update_', 1, 18, '.html')             # 游戏竞技
+    ccuu234.set_seed_url('http://www.uu234.cc/kehuan/list_update_', 1, 32, '.html')             # 科幻灵异
+    ccuu234.set_seed_url('http://www.uu234.cc/shenmei/list_update_', 1, 8, '.html')             # 耽美小说
+    ccuu234.set_seed_url('http://www.uu234.cc/tongren/list_update_', 1, 7, '.html')             # 同人小说
+    ccuu234.set_seed_url('http://www.uu234.cc/qita/list_update_', 1, 65, '.html')               # 其它小说
+    ccuu234.set_seed_url('http://www.uu234.cc/quanben/list_', 1, 390, '.html')                  # 全本书籍
     tpool.set_spider(ccuu234)
     """ www.uu234.cc 结束 """
 
