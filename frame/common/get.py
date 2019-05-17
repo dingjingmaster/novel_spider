39e2d119b7d52400b87ed7b9babb4a79c36470a9
丁敬 <dingjing@live.cn>
2019-05-17 18:32:20 +0800

    更新UA

--- frame/common/get.py
+++ frame/common/get.py
@@ -72,6 +72,11 @@ class Get:
 		'Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)',
 		'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0',
 		'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
+		'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41',
+		'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+		'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)',
 	]
 
 
